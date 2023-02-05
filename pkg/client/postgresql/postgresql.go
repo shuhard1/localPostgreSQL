22f692f43a018906406aaceb72913a4392bb2572
shuhard1 <shuharddog@gmail.com>
2023-02-05 19:57:34 +0300

    First

---
+++ pkg/client/postgresql/postgresql.go
@@ -0,0 +1,42 @@
+package postgresql
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"localPostgreSQL/pkg/utils"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+}
+
+func NewClient(ctx context.Context, maxAttempts int, username, password, host, port, database string) (pool *pgxpool.Pool, err error) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", username, password, host, port, database)
+	err = utils.DoWithTries(func() error {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		pool, err = pgxpool.Connect(ctx, dsn)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, maxAttempts, 5*time.Second)
+
+	if err != nil {
+		log.Fatal("error do with tries postgresql")
+	}
+
+	return pool, nil
+}
