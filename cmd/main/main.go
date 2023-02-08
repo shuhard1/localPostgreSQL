afc513330e7465c5ceeb8889eb335fccfbcefe0f
shuhard1 <shuharddog@gmail.com>
2023-02-09 01:33:04 +0300

    seventh

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/shuhard1/localPostgreSQL/internal/order/db"
@@ -16,15 +15,14 @@ func main() {
 
 	repository := db.NewRepository(conn)
 
-	all, err := repository.FindOne(context.Background(), "b563feb7b2b84b6test")
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-	}
-
 	server := gin.Default()
-	server.GET("/id", func(ctx *gin.Context) {
-		ctx.JSON(200, all.Info)
+	server.GET("/", func(ctx *gin.Context) {
+		id := ctx.Request.URL.Query().Get("id")
+		order, err := repository.FindOne(context.Background(), id)
+		if err != nil {
+			fmt.Fprintf(ctx.Writer, "QueryRow failed: %v\n", err)
+		}
+		ctx.JSON(200, order.Info)
 	})
 	server.Run(":8080")
 }
