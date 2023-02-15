f1ee990ac10be0a80c956bf05991276a29eafe39
shuhard1 <shuharddog@gmail.com>
2023-02-15 23:37:17 +0300

    add html table

---
+++ internal/table/table.go
@@ -0,0 +1,72 @@
+package table
+
+const IndexTmpl = `<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<body>
+<table style="width:100%">
+  <th colspan="2">Order</th>
+  <tr>
+    <td>order_uid</td>
+    <td>{{ .Order_uid }}</td>
+  </tr>
+  <tr>
+    <td>track_number</td>
+    <td>{{ .Track_number }}</td>
+  </tr>
+  <tr>
+    <td>entry</td>
+    <td>{{ .Entry }}</td>
+  </tr>
+  <tr>
+    <td>delivery</td>
+    <td>{{ .Delivery }}</td>
+  </tr>
+  <tr>
+    <td>payment</td>
+    <td>{{ .Payment }}</td>
+  </tr>
+  <tr>
+    <td>items</td>
+    <td>{{ .Items }}</td>
+  </tr>
+  <tr>
+    <td>locale</td>
+    <td>{{ .Locale }}</td>
+  </tr>
+  <tr>
+    <td>internal_signature</td>
+    <td>{{ .Internal_signature }}</td>
+  </tr>
+  <tr>
+    <td>customer_id</td>
+    <td>{{ .Customer_id }}</td>
+  </tr>
+  <tr>
+    <td>delivery_service</td>
+    <td>{{ .Delivery_service }}</td>
+  </tr>
+  <tr>
+    <td>shardkey</td>
+    <td>{{ .Shardkey }}</td>
+  </tr>
+  <tr>
+    <td>sm_id</td>
+    <td>{{ .Sm_id }}</td>
+  </tr>
+  <tr>
+    <td>date_created</td>
+    <td>{{ .Date_created }}</td>
+  </tr>
+  <tr>
+    <td>oof_shard</td>
+    <td>{{ .Oof_shard }}</td>
+  </tr>
+</table>
+</body>
+</html>
+`
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,17 +2,23 @@ package handler
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"text/template"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shuhard1/localPostgreSQL/internal/natsStreaming"
 	"github.com/shuhard1/localPostgreSQL/internal/order"
+	"github.com/shuhard1/localPostgreSQL/internal/table"
 	"github.com/shuhard1/localPostgreSQL/pkg/cache"
 )
 
 type Handler struct {
 	Repositry order.Repository
 }
+
+var index = template.Must(template.New("index").Parse(table.IndexTmpl))
 
 func (h *Handler) InitRouter(c *cache.Cache) *gin.Engine {
 	var err error
@@ -26,12 +32,19 @@ func (h *Handler) InitRouter(c *cache.Cache) *gin.Engine {
 		if !found {
 			ord, err = h.Repositry.FindOne(context.Background(), id)
 			if err != nil {
-				fmt.Fprintf(ctx.Writer, "QueryRow failed: %v\n", err)
+				fmt.Fprintf(ctx.Writer, "FindOne failed: %v\n", err)
 			} else {
-				c.Set(ord.ID, ord.Info+" from cache", 5*time.Minute)
+				c.Set(ord.ID, ord.Info, 5*time.Minute)
 			}
 		}
-		ctx.JSON(200, ord.Info)
+		var dat natsStreaming.Order
+		err := json.Unmarshal([]byte(ord.Info), &dat)
+		if err != nil {
+			fmt.Fprintf(ctx.Writer, "unmarshal error: %s\n", err)
+		}
+		if err := index.Execute(ctx.Writer, dat); err != nil {
+			fmt.Fprintf(ctx.Writer, "index.Execute error: %s\n", err)
+		}
 	})
 	router.Run(":8080")
 
